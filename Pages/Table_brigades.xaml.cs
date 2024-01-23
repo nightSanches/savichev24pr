4dc379b4f4b4508936635ff5e5a3dc02e9c46afc
gsavichev <galesavich@gmail.com>
2024-01-23 23:47:55 +0500

    Разработана форма таблицы Бригады

---
+++ Pages/Table_brigades.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace savichev24pr.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Table_brigades.xaml
+    /// </summary>
+    public partial class Table_brigades : Page
+    {
+        public Table_brigades()
+        {
+            InitializeComponent();
+        }
+    }
+}
