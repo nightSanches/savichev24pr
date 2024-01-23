47485a801e0ef189dd778261db904c17aa814cb7
gsavichev <galesavich@gmail.com>
2024-01-23 23:44:10 +0500

    Разработана форма таблицы Льготы и Расписание вылетов

---
+++ Pages/Table_raspisanye.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace savichev24pr.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Table_raspisanye.xaml
+    /// </summary>
+    public partial class Table_raspisanye : Page
+    {
+        public Table_raspisanye()
+        {
+            InitializeComponent();
+        }
+    }
+}
