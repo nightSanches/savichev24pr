9f531f52e8de178caf1e309c9648655a5bccc704
gsavichev <galesavich@gmail.com>
2024-01-23 23:58:15 +0500

    Разработанно окно редактирования/добавления записи в таблицу Бригады

---
+++ Windows/Add_edit_brigades.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace savichev24pr.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_edit_brigades.xaml
+    /// </summary>
+    public partial class Add_edit_brigades : Window
+    {
+        public Add_edit_brigades()
+        {
+            InitializeComponent();
+        }
+    }
+}
