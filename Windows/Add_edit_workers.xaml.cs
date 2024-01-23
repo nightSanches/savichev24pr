017f57e1b4b4c965a77e7d6e9c1feda8d1742698
gsavichev <galesavich@gmail.com>
2024-01-24 00:00:19 +0500

    Разработанно окно редактирования/добавления записи в таблицу Работники

---
+++ Windows/Add_edit_workers.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace savichev24pr.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_edit_workers.xaml
+    /// </summary>
+    public partial class Add_edit_workers : Window
+    {
+        public Add_edit_workers()
+        {
+            InitializeComponent();
+        }
+    }
+}
